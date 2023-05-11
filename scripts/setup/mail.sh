3a6af8f156495ce2fdce688a02a92bc255be7db1
erkerc <erkan.ercan@gmail.com>
2023-05-11 15:35:09 +0300

    Last working version in OCP 4.9

--- scripts/setup/mail.sh
+++ scripts/setup/mail.sh
@@ -5,7 +5,7 @@ mailhost="http://standalone-demo-mail.apps.cluster-cb6z5.cb6z5.sandbox754.opentl
 curl -X POST $mailhost"/api/service/reset" \
  -H "Accept: application/json" 
 
-curl -VX POST $mailhost"/api/user" \
+curl -X POST $mailhost"/api/user" \
  -H "Accept: application/json" \
  -H "Content-Type: application/json" \
  -d '{"email":"strategy@demo.camelk","login":"strategy","password":"demo"}'
